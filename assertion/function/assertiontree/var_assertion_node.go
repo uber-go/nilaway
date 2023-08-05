ee7147b239c7b8dc1919349153ca3fe45732079c
sonalmahajan15 <smahajan@uber.com>
2023-08-04 20:37:21 -0700

    simplify BuildExpr()

--- assertion/function/assertiontree/var_assertion_node.go
+++ assertion/function/assertiontree/var_assertion_node.go
@@ -21,7 +21,6 @@ import (
 
 	"go.uber.org/nilaway/annotation"
 	"go.uber.org/nilaway/util"
-	"golang.org/x/tools/go/analysis"
 )
 
 type varAssertionNode struct {
@@ -77,7 +76,7 @@ func (v *varAssertionNode) DefaultTrigger() annotation.ProducingAnnotationTrigge
 }
 
 // BuildExpr for a varAssertionNode returns the underlying variable's AST node
-func (v *varAssertionNode) BuildExpr(_ *analysis.Pass, _ ast.Expr) ast.Expr {
+func (v *varAssertionNode) BuildExpr() ast.Expr {
 	if v.Root() == nil {
 		panic("v.BuildExpr should only be called on nodes present in a valid assertion tree")
 	}
