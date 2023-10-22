39fd48b4369b9b49aa9b1a56785b46cf93a0378f
sonalmahajan15 <smahajan@uber.com>
2023-10-21 17:10:09 -0700

    add comments

--- assertion/function/assertiontree/root_assertion_node.go
+++ assertion/function/assertiontree/root_assertion_node.go
@@ -674,28 +674,33 @@ func (r *RootAssertionNode) AddComputation(expr ast.Expr) {
 					}
 					r.AddConsumption(&consumer)
 
+					// If arg is a deep type, we add a full trigger for it to track its deep nilability.
+					// ```
+					// E.g., func bar(s []*int) {
+					//   foo(s) // <-- track shallow and deep nilability of `s` here
+					// }
+					// ```
 					if util.TypeIsDeep(util.TypeOf(r.Pass(), arg)) {
-						if ident, ok := arg.(*ast.Ident); ok {
-							v := r.ObjectOf(ident).(*types.Var)
-							deepProducer := &annotation.ProduceTrigger{
-								Annotation: annotation.DeepNilabilityOfVar(r.FuncObj(), v),
-								Expr:       arg,
-							}
-							deepConsumer := &annotation.ConsumeTrigger{
-								Annotation: &annotation.ArgPassDeep{
-									TriggerIfDeepNonNil: &annotation.TriggerIfDeepNonNil{
-										Ann: paramKey,
-									}},
-								Expr:   arg,
-								Guards: util.NoGuards(),
-							}
-							deepConsumer.Annotation.SetNeedsGuard(false)
-
-							r.AddNewTriggers(annotation.FullTrigger{
-								Producer: deepProducer,
-								Consumer: deepConsumer,
-							})
+						deepProducer := &annotation.ProduceTrigger{
+							Annotation: exprAsDeepProducer(r, arg),
+							Expr:       arg,
 						}
+						deepConsumer := &annotation.ConsumeTrigger{
+							Annotation: &annotation.ArgPassDeep{
+								TriggerIfDeepNonNil: &annotation.TriggerIfDeepNonNil{
+									Ann: paramKey,
+								}},
+							Expr:   arg,
+							Guards: util.NoGuards(),
+						}
+						// since this is an implicit tracking of the deep nilability of arg, we don't need to
+						// check for its guarding
+						deepConsumer.Annotation.SetNeedsGuard(false)
+
+						r.AddNewTriggers(annotation.FullTrigger{
+							Producer: deepProducer,
+							Consumer: deepConsumer,
+						})
 					}
 				}
 			}
