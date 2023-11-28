6e6f5a59cd09c31c7f258cbd9c355b03dd7ae3fb
Yuxin Wang <yuxinw@uber.com>
2023-11-28 11:11:18 -0500

    Remove unnecessary docstring

--- assertion/function/functioncontracts/testdata/src/go.uber.org/functioncontracts/main.go
+++ assertion/function/functioncontracts/testdata/src/go.uber.org/functioncontracts/main.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// <nilaway contract enable>
 package functioncontracts
 
 // contract(nonnil -> nonnil)
