bc38ecd38f28fb607db7f82c8d04ac77779f8fb3
sonalmahajan15 <smahajan@uber.com>
2024-01-17 14:04:38 -0800

    rebase fixes

--- testdata/src/go.uber.org/errormessage/errormessage.go
+++ testdata/src/go.uber.org/errormessage/errormessage.go
@@ -124,27 +124,12 @@ func test12(mp map[int]S, i int) {
 	y := mp[i] // unrelated assignment, should not be printed in the error message
 	_ = y
 
-	s := mp[i] // relevant assignment, should be printed in the error message
-	consumeS(&s)
+	s := mp[i]   // relevant assignment, should be printed in the error message
+	consumeS(&s) //want "`mp\\[i\\]` to `s`"
 }
 
 func consumeS(s *S) {
-	print(s.f) //want "`mp\\[i\\]` to `s`"
-}
-
-func retErr() error {
-	return errors.New("error")
-}
-
-func test13() *int {
-	if err := retErr(); err != nil { // unrelated assignment, should not be printed in the error message
-		return nil
-	}
-	return new(int)
-}
-
-func callTest13() {
-	print(*test13()) //want "literal `nil` returned"
+	print(s.f)
 }
 
 func retErr() error {
