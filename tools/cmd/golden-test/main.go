59daca881735f0f4a403d115c4e0b37778673fe4
Yuxin Wang <yuxinw@uber.com>
2024-03-14 18:03:50 -0400

    Add linting for copyright headers (#211)

--- tools/cmd/golden-test/main.go
+++ tools/cmd/golden-test/main.go
@@ -1,3 +1,17 @@
+//  Copyright (c) 2023 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package main implements the golden tests for NilAway to ensure that the errors reported on the
 // stdlib are equal between the base branch and the test branch for preventing functionality
 // regressions during development.
