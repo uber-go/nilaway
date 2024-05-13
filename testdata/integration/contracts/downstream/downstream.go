831eca7a7616e05393b8ea8eb75c4addde0e38b9
Yuxin Wang <yuxinw@uber.com>
2024-05-13 14:48:02 -0400

    update

--- testdata/integration/contracts/downstream/downstream.go
+++ testdata/integration/contracts/downstream/downstream.go
@@ -28,5 +28,7 @@ func GiveUpstreamNil() {
 func GiveUpstreamNonnil() {
 	a := 1
 	r := upstream.NonnilToNonnil(&a)
-	print(*r) // Safe!
+	// TODO: FP: this should be safe due to the contract. We should remove this once contract
+	//  support is fixed in NilAway.
+	print(*r) //want "result 0 of `NonnilToNonnil\(\)` dereferenced"
 }
