2ae4ebe994093686153e034713d09f67943e3d86
Yuxin Wang <yuxinw@uber.com>
2024-05-24 15:22:41 -0400

    Refactor CFG preprocess logic (#249)
    This PR refactors the CFG preprocess logic. Specifically, we have
    extracted the rich check effect construction out of the preprocessing
    logic such that it is now in its standalone file. This offers a lot
    better modularity for the already-enormous `assertiontree` package.
    
    Now in `BackpropAcrossFunc` function we are calling preprocess and
    rich-check-effect-generating functions separately.

--- assertion/function/assertiontree/backprop.go
+++ assertion/function/assertiontree/backprop.go
@@ -814,11 +814,18 @@ func computePostOrder(blocks []*cfg.Block) []int {
 // BackpropAcrossFunc is the main driver of the backpropagation, it takes a function declaration
 // with accompanying CFG, and back-propagates a tree of assertions across it to generate, at entry
 // to the function, the set of assertions that must hold to avoid possible nil flow errors.
-func BackpropAcrossFunc(ctx context.Context, pass *analysis.Pass, decl *ast.FuncDecl,
-	functionContext FunctionContext, graph *cfg.CFG) ([]annotation.FullTrigger, int, int, error) {
+func BackpropAcrossFunc(
+	ctx context.Context,
+	pass *analysis.Pass,
+	decl *ast.FuncDecl,
+	functionContext FunctionContext,
+	graph *cfg.CFG,
+) ([]annotation.FullTrigger, int, int, error) {
 	// We transform the CFG to have it reflect the implicit control flow that happens
 	// inside short-circuiting boolean expressions.
-	graph, richCheckBlocks, exprNonceMap := preprocess(graph, functionContext)
+	graph = preprocess(graph, functionContext.funcDecl, functionContext.pass)
+	richCheckBlocks, exprNonceMap := genInitialRichCheckEffects(graph, functionContext)
+	richCheckBlocks = propagateRichChecks(graph, richCheckBlocks)
 	blocks, preprocessing := blocksAndPreprocessingFromCFG(pass, graph, richCheckBlocks)
 
 	// The assertion nodes for each block and an array of bools to indicate whether each block is
