bd3e24ec106aa2f92b94f1bea277ec7688a3493a
sonalmahajan15 <smahajan@uber.com>
2024-12-18 22:49:55 -0800

    testing

--- assertion/function/assertiontree/backprop.go
+++ assertion/function/assertiontree/backprop.go
@@ -197,6 +197,11 @@ func backpropAcrossReturn(rootNode *RootAssertionNode, node *ast.ReturnStmt) err
 				// we've identified that a multiply-returning function is being returned
 
 				_, producers := rootNode.ParseExprAsProducer(call, true)
+				if util.FuncNumResults(funcObj) != len(producers) {
+					// this should never happen - if it does, it's a bug in the producer parser
+					return errors.New(fmt.Sprintf("function %s has %d results but %d producers were found", fident.Name, util.FuncNumResults(funcObj), len(producers)))
+				}
+
 				for i := 0; i < util.FuncNumResults(funcObj); i++ {
 					if producers == nil {
 						// this nil check reflects programmer logic
