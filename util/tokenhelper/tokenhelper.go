98ca12694d0335828ec7a0ff62ee4ba1849209a9
Yuxin Wang <yuxinw@uber.com>
2025-04-10 21:30:32 -0400

    Update

--- util/tokenhelper/tokenhelper.go
+++ util/tokenhelper/tokenhelper.go
@@ -21,18 +21,15 @@ import (
 	"path/filepath"
 )
 
-var _cwd = func() string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic("failed to get current working directory: " + err.Error())
-	}
-	return cwd
-}()
+var _cwd, _cwdErr = os.Getwd()
 
 // RelToCwd returns the relative path of the given filename with respect to the current
 // working directory (retrieved during initialization). If the filename is not a child of
 // the current working directory, it returns the filename itself.
 func RelToCwd(filename string) string {
+	if _cwdErr != nil {
+		panic("failed to get current working directory: " + _cwdErr.Error())
+	}
 	rel, err := filepath.Rel(_cwd, filename)
 	if err == nil {
 		return rel
