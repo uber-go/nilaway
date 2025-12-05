dac1db1cef8e11f82457116fdb1b5ba65d0f98d5
sonalmahajan15 <smahajan@uber.com>
2025-12-05 15:05:11 -0800

    test

--- assertion/function/assertiontree/util.go
+++ assertion/function/assertiontree/util.go
@@ -333,12 +333,11 @@ func likelyPositiveInt(pass *analysishelper.EnhancedPass, expr ast.Expr) bool {
 	if v, ok := pass.ConstInt(expr); ok {
 		return v > 0
 	}
-	exprType := pass.TypesInfo.TypeOf(expr)
-	if exprType == nil {
-		return false
-	}
-	if t, ok := exprType.Underlying().(*types.Basic); ok {
-		return t.Info()&types.IsInteger != 0
+
+	if _, ok := expr.(*ast.BasicLit); ok {
+		if t, ok := pass.TypesInfo.TypeOf(expr).Underlying().(*types.Basic); ok {
+			return t.Info()&types.IsInteger != 0
+		}
 	}
 	return false
 }
