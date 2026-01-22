f83a53a0bb8cf3bae929e82ec3c324abdfbb3d1a
Yuxin Wang <yuxinw@uber.com>
2026-01-22 12:43:01 -0500

    Update

--- util/guard_nonce.go
+++ util/guard_nonce.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package util hosts the guard nonce types and functions to identify and track RichCheckEffects.
 package util
 
 import (
